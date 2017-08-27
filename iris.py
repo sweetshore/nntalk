04dcacdb6d26fc76a701dd61c470b883c652cbd9
iburiak <iburiak@evernote.com>
2017-08-27 17:13:26 +0300

    optimization part completed

---
+++ iris.py
@@ -0,0 +1,10 @@
+from __future__ import print_function
+import matplotlib.pyplot as plt
+import numpy as np
+
+def read_iris_data(fname):
+    pass
+
+
+if __name__ == "__main__":
+    pass
--- optimization.py
+++ optimization.py
@@ -1,4 +1,11 @@
-from matplotlib import pyplot as plt
+from __future__ import print_function
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from matplotlib import cm
+#from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import numpy as np
+
+### 1-d optimization 
 
 def profit_fun(x):
     """Sample profit function for cat house business"""
@@ -8,7 +15,7 @@ def profit_fun(x):
     return profit
 
 def grid_search(fun, x_min, x_max, num_steps):
-    x_step = (x_max - x_min) / num_steps
+    x_step = (x_max - x_min) / (num_steps - 1)
     steps = []
     best_value = -1e100
     best_x = x_min
@@ -40,25 +47,57 @@ def constant_step_search(fun, x_start, x_step, num_steps):
     return x, fun(x), steps
 
 
-def gradient(fun, x, eps):
-    """Returns function grow rate (aka gradient)"""
-    f_plus = fun(x + eps)
-    f_minus = fun(x - eps)
-    return (f_plus - f_minus) / (2 * eps)
+def gradient_1d(fun, x, eps):
+    """Returns function grow rate (aka gradient) for 1-variable functions"""
+    return (fun(x + eps) - fun(x - eps)) / (2 * eps)
 
-def gradient_search(fun, x_start, rate, num_steps):
-    steps = []
+def gradient_search_1d(fun, x_start, rate, num_steps):
+    """Search for 1-d function maximum by gradient descent"""
+    steps = [] 
     x = x_start
+    steps.append((x, fun(x))) # add x-start
     for n in range(num_steps):
-        grad = gradient(fun, x, 0.01)
+        grad = gradient_1d(fun, x, 0.01)
         x += rate * grad
         steps.append((x, fun(x)))
     return x, fun(x), steps
 
+### 2-d optimization 
+
+def loss_fun(x):
+    """Sample loss function for two variable problem""" 
+    x1 = x[0]
+    x2 = x[1]
+    f = 5 * (x1 - 4)**2 + 3 * (x2 - 5)**2 
+    #f = 20 * (x1 - 4)**2 + 3 * (x2 - 5)**2 
+    return f
+
+def gradient(fun, x, eps):
+    """Search for function minimum by gradient descent"""
+    grad = np.zeros(len(x)) # array with zeros [0 0 .. 0] same size as x
+    for n in range(len(x)):
+        x_plus = np.copy(x) # copy values of array, not reference to old array
+        x_plus[n] += eps
+        x_minus = np.copy(x)
+        x_minus[n] -= eps
+        grad[n] = (fun(x_plus) - fun(x_minus)) / (2 * eps)
+    return grad
+
+def gradient_search(fun, x_start, rate, num_steps):
+    steps = []
+    x = x_start
+    steps.append((np.copy(x), fun(x)))
+    for n in range(num_steps):
+        grad = gradient(fun, x, 0.01)
+        x -= rate * grad
+        steps.append((np.copy(x), fun(x)))
+    return x, fun(x), steps
+
+### plot functions
 
 def plot_search_steps(fun, x_min, x_max, steps):
     # plot smooth curve in range
-    plot_points = 1000
+    plot_points = 100
     x_step = (x_max - x_min) / plot_points
     x = []
     y = []
@@ -79,11 +118,62 @@ def plot_search_steps(fun, x_min, x_max, steps):
     # display plot
     plt.show()
 
+def plot_search_steps_3d(fun, x_min, x_max, steps = []):
+    
+    plot_points = 100
+    x_step = (x_max - x_min) / plot_points
+
+    x = np.arange(x_min, x_max, x_step)
+    y = np.arange(x_min, x_max, x_step)
+    x, y = np.meshgrid(x, y)
+    z = [fun([x[n], y[n]]) for n in range(len(x))]
+
+    steps_x = []
+    steps_y = []
+    steps_z = []
+    for point, val in steps:
+        steps_x.append(point[0])
+        steps_y.append(point[1])
+        steps_z.append(val)
+            
+    # display plot
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+
+    # plot search steps    
+    ax.plot(steps_x, steps_y, zs=steps_z, color='r', linestyle='solid', marker='o')
+
+    # plot the surface
+    surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+
+
+    ax.set_xlabel("x_1")
+    ax.set_ylabel("x_2")
+
+    # Customize the z axis.
+    #ax.set_zlim(-1.01, 1.01)
+    #ax.zaxis.set_major_locator(LinearLocator(10))
+    #ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+
+    # Add a color bar which maps values to colors.
+    #fig.colorbar(surf, shrink=0.5, aspect=5)
+
+    plt.show()
 
 if __name__ == "__main__":
-    #best_x, best_value, steps = grid_search(profit_fun, 0, 200, 30)
+    
+    # 1-d optimization
+    best_x, best_value, steps = grid_search(profit_fun, 0, 200, 30)
     #best_x, best_value, steps = constant_step_search(profit_fun, 100, 1, 30)
-    best_x, best_value, steps = gradient_search(profit_fun, 100, 10, 30)
+    #best_x, best_value, steps = gradient_search_1d(profit_fun, 100, 10, 30)
 
     print("solution", best_x, best_value)
     plot_search_steps(profit_fun, 0, 200, steps)
+
+    # 2-d optimization
+    #x_start = np.array([9.0, 9.0])
+    #rate = 0.05 #0.01
+    #best_x, best_value, steps = gradient_search(loss_fun, x_start, rate, 30)
+    #print("solution", best_x, best_value)
+    #plot_search_steps_3d(loss_fun, 0.0, 10.0, steps)
